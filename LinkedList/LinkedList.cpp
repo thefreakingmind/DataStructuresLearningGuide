a2a9238d01c58e4e48d50329e1c574438730118f
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-07-16 23:14:17 +0530

    Fixed a Bug

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -11,9 +11,10 @@ struct Node *head;
 
 void Insert(int x)
 {
-  Node *Temp = new Node();
+  Node *Temp = new Node;
   Temp->data = x;
   Temp->next = NULL;
+  if (head!=NULL) Temp->next= head;
   head = Temp;
 }
 void print()
@@ -36,7 +37,7 @@ int main()
   int n;
   cout << "How Many Number You want to Enter" << endl;
   cin >> n;
-  cout << "Enter The List
+  cout << "Enter The List";
   for(int i=0; i<n; i++)
   {
 	cin >> x;
