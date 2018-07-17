1093683be38840df44e28518eb28a293c0321b89
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-07-17 09:06:03 +0530

    Fixed

--- Pointers/Pointer_Array.cpp
+++ Pointers/Pointer_Array.cpp
@@ -6,6 +6,7 @@ int main()
   int arr[4] = {1,2,3,4};
   for(int i=0; i<=4; i++)
   {
-	cout << &arr[i]<<endl;
+	cout << "Address: " <<  &arr[i] << endl;
+	cout << "Values: " << *(arr+i) << endl;
   }
 }
