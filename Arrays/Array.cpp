d1fc8823ca8230ad1c2d2e2fb1966221199df0ff
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-07-23 13:40:22 +0530

    Delete Linked List

---
+++ Arrays/Array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int arr[10];
+  for(int i=0; i<=10; i++)
+  {
+	cin >> arr[i];
+  }
+  for(int j=0; j<=10; j++)
+  {
+	cout << arr[j] << endl;
+  }
+  return 0;
+}
+
---
+++ Arrays/MultiDim.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int arr[4][3];
+  for(int i=0; i<=4; i++)
+  {
+	for(int i=0; i<=3; i++)
+	{
+	  cout << "Enter The Column" << endl;
+	  cin >> arr[j];
+	}
+  }
