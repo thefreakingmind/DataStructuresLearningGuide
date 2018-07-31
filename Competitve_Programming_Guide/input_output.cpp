7b939ac36a5abf4d52feee8f0d990656f47e9acc
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-07-31 12:29:45 +0530

    Update 1

---
+++ Competitve_Programming_Guide/input_output.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+  
+int main()
+{
+  int a = 323;
+  int b = 500;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string s;
+  getline(cin, s);
+  cout << s << "\n";
+  cout << a+b << "\n";
+}
