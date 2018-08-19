c3d8ed9b6ee4190e7b352702514d155dca2c3925
Salman Siddiqui <salmansiddiqui100@gmail.com>
2018-08-19 23:35:52 +0530

    Python Added

---
+++ LinkedList/Python/Node.py
@@ -0,0 +1,15 @@
+#Creating a Basic Node
+class Node:
+    def __init__(self, value, link=None):
+        self.value = value
+        self.link = link
+
+        #getting a value from node
+        def get_value(self):
+            return self.value
+
+        def get_link_node(self):
+            return self.link
+
+        def set_link_node(self, link):
+            return self.link = link
